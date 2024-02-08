599c641276b88edf9a4b294d67dad62104db8663
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2024-02-08 11:30:24 -0300

    fix: lambda runs!

--- src/main.rs
+++ src/main.rs
@@ -7,25 +7,17 @@ mod put_object;
 mod response;
 use put_object::put_object;
 
-
 #[tokio::main]
 async fn main() -> Result<(), Error> {
     tracing_subscriber::fmt()
         .with_max_level(tracing::Level::INFO)
-        // disable printing the name of the module in every log line.
         .with_target(false)
-        // disabling time is handy because CloudWatch will add the ingestion time.
         .without_time()
         .init();
 
     let bucket_name = std::env::var("BUCKET_NAME")
         .expect("A BUCKET_NAME must be set in this app's Lambda environment variables.");
 
-    // Initialize the client here to be able to reuse it across
-    // different invocations.
-    //
-    // No extra configuration is needed as long as your Lambda has
-    // the necessary permissions attached to its role.
     let config = aws_config::load_defaults(BehaviorVersion::latest()).await;
     let s3_client = aws_sdk_s3::Client::new(&config);
 
