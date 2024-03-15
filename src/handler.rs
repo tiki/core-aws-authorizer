bd88ad6e11975f2ec93e0078d0414f75ff8895e2
Mike Audi <mike@mytiki.com>
2024-03-15 13:37:30 -0500

    fix: add scopes to response, all err denied

--- src/handler.rs
+++ src/handler.rs
@@ -13,14 +13,27 @@ use aws_lambda_events::apigw::{
 
 pub async fn entry(
     event: LambdaEvent<ApiGatewayCustomAuthorizerRequestTypeRequest>
-) -> Result<ApiGatewayCustomAuthorizerResponse<ResponseContext>, Box<dyn Error>> {
+) -> Result<ApiGatewayCustomAuthorizerResponse<ResponseContext>, lambda_runtime::Error> {
+    tracing::debug!("{:?}", event);
+    let res = process(&event).await
+        .map_err(|err| { 
+            tracing::error!("{:?}", err);
+            err 
+        });
+    Ok(ResponseBuilder::default().for_event(event).with_result(res).build())
+}
+
+async fn process(
+    event: &LambdaEvent<ApiGatewayCustomAuthorizerRequestTypeRequest>
+) -> Result<ResponseContext, Box<dyn Error>> {
     let token = event.payload.headers.get("Authorization").ok_or("No authorization header")?.to_str()?;
     let audience = event.payload.path.as_ref().ok_or("No event path")?.to_string();
     let validate = Validate::new_from_env(&audience)?;
     let token = validate.decode(token)?;
     let claims = validate.claims(&token)?;
     let sub = claims.custom.sub().clone().ok_or("Missing sub")?;
-    let res = ResponseContext::from_subject(&sub);
-    ResponseBuilder::default().for_event(event).with_result(res).build()
+    let context = ResponseContext::from_subject(&sub)?
+        .with_scopes(&mut claims.custom.scp().clone().unwrap_or(vec![]));
+    Ok(context)
 }
 
--- src/validate/response_context.rs
+++ src/validate/response_context.rs
@@ -8,22 +8,39 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug)]
 pub struct ResponseContext{
-    role: String,
+    namespace: String,
     id: String,
+    scopes: Vec<String>
 }
 
 impl ResponseContext {
-    pub fn new(role: &str, id: &str) -> Self { Self { role: role.to_string(), id: id.to_string() } }
+    
+    pub fn default() -> Self { Self::new("","") }
+    pub fn new(namespace: &str, id: &str) -> Self { 
+        Self { 
+            namespace: namespace.to_string(), 
+            id: id.to_string(),
+            scopes: vec![]
+        } 
+    }
 
-    pub fn role(&self) -> &str { &self.role }
+    pub fn namespace(&self) -> &str { &self.namespace }
     pub fn id(&self) -> &str { &self.id }
-
-    pub fn to_principal(&self) -> String { [self.role.to_string(), self.id.to_string()].join(":") }
+    pub fn scopes(&self) -> &Vec<String> { &self.scopes }
+    
+    pub fn to_principal(&self) -> String { [self.namespace.to_string(), self.id.to_string()].join(":") }
     pub fn from_subject(subject: &str) -> Result<Self, Box<dyn Error>> {
         let mut split = subject.splitn(2, ':');
         Ok(Self {
-            role: split.next().ok_or("Subject missing role")?.to_string(),
-            id: split.next().ok_or("Subject missing id")?.to_string()
+            namespace: split.next().ok_or("Subject missing role")?.to_string(),
+            id: split.next().ok_or("Subject missing id")?.to_string(),
+            scopes: vec![]
         })
+    }
+    pub fn with_scopes(mut self, scopes: &mut Vec<String>) -> Self {
+        let mut scp = self.scopes;
+        scp.append(scopes);
+        self.scopes = scp;
+        self
     }
 }
